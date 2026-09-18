876c505c83d5c3eabe6f2efec0278ba71aab0f1b
wsehsatnoen <113001014+wsehsatnoen@users.noreply.github.com>
2026-09-18 12:04:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 
 int main() {
 	
-	// My Information:
-	cout << "Course    : Scripting and Programming - Applications - C867\n";
-	cout << "Language  : C++\n";
-	cout << "StudentID : 012336566\n";
-	cout << "Name      : Shane Darilek\n\n";
-	
 	// Building the Roster:
 	const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Shane,Darilek,swdarilek@gmail.com,24,20,20,18,SOFTWARE" };
 	Roster classRoster(studentData, 5, 20);
